b1d639606181fda154e78f0f2eda34f7dd53426a
Imggaggu <ggaggu@snu.ac.kr>
2025-03-04 02:19:39 +0900

    섹션3-7강 문제와 풀이

---
+++ JAVA/inflearn_javastart/javastart/src/variable/VarEx3.java
@@ -0,0 +1,16 @@
+package variable;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+        long L;
+        L=10000000000L;
+        System.out.println(L);
+        boolean b=true;
+        System.out.println(b);
+
+    }
+
+
+}
