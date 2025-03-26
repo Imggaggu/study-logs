13a293a832a03624b55c18b2ae7e4976aaec487e
Imggaggu <ggaggu@snu.ac.kr>
2025-03-26 20:20:34 +0900

    cppstart-섹션2-1강 변수 선언과 규칙

---
+++ C++/cpp_start/section2_dealing_with_data/data_declaration.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+  int a;
+  int b=38;
+  a=3;
+
+  cout <<"a="<< a <<endl;
+  cout<<"b="<< b <<endl;
+  
+  cout <<"a address="<< &a <<endl;
+  cout<<"b address="<< &b <<endl;
+  return 0;
+  
+
+}
