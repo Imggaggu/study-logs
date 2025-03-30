480bcca46098010bac607cae20e3267d953a84f9
Imggaggu <ggaggu@snu.ac.kr>
2025-03-30 14:51:53 +0900

    거북이 9마리 생성!

--- ROS2/ros2_ws/src/my_first_pkg/my_first_pkg/my_service_server.py
+++ ROS2/ros2_ws/src/my_first_pkg/my_first_pkg/my_service_server.py
@@ -1,5 +1,9 @@
 from my_first_pkg_msgs.srv import MultiSpawn
 from turtlesim.srv import TeleportAbsolute
+from turtlesim.srv import Spawn
+
+import time
+import numpy as np
 import rclpy as rp
 from rclpy.node import Node
 
@@ -8,12 +12,34 @@ class MultiSpawning(Node):
     super().__init__('multi_spawn')
     self.server=self.create_service(MultiSpawn,'multi_spawn',self.callback_service)
     self.teleport =self.create_client(TeleportAbsolute, "/turtle/teleport_absolute")
+    self.spawn = self.create_client(Spawn,'/spawn')
     self.req_teleport = TeleportAbsolute.Request()
-    
+    self.req_spawn=Spawn.Request()
+
+    self.center_x=5.54
+    self.center_y=5.54
+
+  def calc_position(self, n, r):
+    if n!=0:
+      gap_theta = 2. *np.pi / n
+      theta=[gap_theta*n for n in range(n)]
+      x=[r*np.cos(th) for th in theta]
+      y=[r*np.sin(th) for th in theta]
+
+    return x, y, theta
+
   def callback_service(self, request, response):
-    self.req_teleport.x = 1.0
-    self.req_teleport.y = 1.0
-    self.teleport.call_async(self.req_teleport)
+    x,y,theta = self.calc_position(request.num,3)
+    for n in range(len(theta)):
+      self.req_spawn.x=x[n]+self.center_x
+      self.req_spawn.y=y[n]+self.center_y
+      self.req_spawn.theta=theta[n]
+
+      self.spawn.call_async(self.req_spawn)
+      
+    response.x=x
+    response.y=y
+    response.theta=theta
     return response
     
    # response.x=[1.,2.,3.]
